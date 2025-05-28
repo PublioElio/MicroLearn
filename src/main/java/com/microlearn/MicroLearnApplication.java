9b919a9302865fd3fc802cac78fe30588cb30622
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-05-28 10:55:02 +0200

    Component Scan added

--- src/main/java/com/microlearn/MicroLearnApplication.java
+++ src/main/java/com/microlearn/MicroLearnApplication.java
@@ -2,13 +2,14 @@ package com.microlearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.microlearn.controllers" })
 public class MicroLearnApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroLearnApplication.class, args);
 	}
 
-	
 }
