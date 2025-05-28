f6e5ea15e163f63ae7cb1a1a6bd79938c5fa8186
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-05-28 10:43:45 +0200

    Generated Getters and Setters in Course class

--- src/main/java/com/microlearn/model/Course.java
+++ src/main/java/com/microlearn/model/Course.java
@@ -4,9 +4,33 @@ public class Course {
 	private String title;
 	private int length;
 	private String schedule;
-	
+
 	public Course() {
-		
+
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public String getSchedule() {
+		return schedule;
+	}
+
+	public void setSchedule(String schedule) {
+		this.schedule = schedule;
 	}
 
 }
