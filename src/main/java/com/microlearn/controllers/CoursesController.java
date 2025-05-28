2e19923746639fc33e9ab505eb1b1d7b687d24dc
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-05-28 10:59:57 +0200

    Added Post Construct

--- src/main/java/com/microlearn/controllers/CoursesController.java
+++ src/main/java/com/microlearn/controllers/CoursesController.java
@@ -1,13 +1,30 @@
 package com.microlearn.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microlearn.model.Course;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 public class CoursesController {
+
+	private List<Course> courses;
+
+	@PostConstruct
+	public void init() {
+		courses = new ArrayList<>();
+		courses.add(new Course("Spring Boot", 150, "Tarde"));
+		courses.add(new Course("Python", 300, "Tarde"));
+		courses.add(new Course("Java EE", 250, "Mañana"));
+		courses.add(new Course("Java Script", 150, "Tarde"));
+		courses.add(new Course("Linux", 80, "Finde"));
+	}
 
 	@GetMapping(value = "course", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Course getCourse() {
