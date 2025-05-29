b5a414df92324ee5b7082ce73a5dd0f50c4b6e1f
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-05-29 08:54:27 +0200

    First test

--- src/test/java/com/microlearn/MicroLearnApplicationTests.java
+++ src/test/java/com/microlearn/MicroLearnApplicationTests.java
@@ -1,5 +1,8 @@
 package com.microlearn;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,7 +17,8 @@ class MicroLearnApplicationTests {
 	MockMvc mock;
 
 	@Test
-	void contextLoads() {
+	void testCourses() throws Exception {
+		mock.perform(get("/courses")).andDo(print());
 	}
 
 }
