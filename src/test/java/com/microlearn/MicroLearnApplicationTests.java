93a09e667c915229236a965bb6c019f788c9bbf6
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-05-29 09:09:19 +0200

    Improvements in test class

--- src/test/java/com/microlearn/MicroLearnApplicationTests.java
+++ src/test/java/com/microlearn/MicroLearnApplicationTests.java
@@ -1,24 +1,37 @@
 package com.microlearn;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureMockMvc
+@TestMethodOrder(OrderAnnotation.class)
 @SpringBootTest
 class MicroLearnApplicationTests {
-	
+
 	@Autowired
 	MockMvc mock;
 
 	@Test
+	@Order(0)
+	void deleteCourse() throws Exception {
+		mock.perform(delete("/courses/Python")).andDo(print());
+	}
+
+	@Test
+	@Order(1)
 	void testCourses() throws Exception {
-		mock.perform(get("/courses")).andDo(print());
+		mock.perform(get("/courses")).andDo(print()).andExpect(status().isOk());
 	}
 
 }
