907e120a203d2902fa95d8637857e3ef56d54cbe
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2025-06-05 00:27:37 +0200

    comments on MicroLearnApplication class

--- src/main/java/com/microlearn/MicroLearnApplication.java
+++ src/main/java/com/microlearn/MicroLearnApplication.java
@@ -4,10 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * Entry point for the MicroLearn application. This class initializes the Spring
+ * Boot framework and configures component scanning.
+ * 
+ * @author Adriano
+ */
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.microlearn.controllers" })
 public class MicroLearnApplication {
 
+	/**
+	 * Main method that starts the MicroLearn application. It uses
+	 * {@link SpringApplication#run(Class, String...)} to bootstrap the application.
+	 * 
+	 * @param args Command-line arguments for the application (optional).
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(MicroLearnApplication.class, args);
 	}
